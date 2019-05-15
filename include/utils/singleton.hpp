d48bbbd4baf62fd7cdaef34759094c65c8346d99
Geoffrey ROYER <geoffrey.royer@gmail.com>
2019-05-15 20:01:34 +0200

    Use singleton

---
+++ include/utils/singleton.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+template<typename T>
+class Singleton {
+public:
+    static T& instance() {
+        return m_instance;
+    }
+private:
+    static T m_instance;
+};
+
+
+template <class T> T Singleton<T>::m_instance = T();
