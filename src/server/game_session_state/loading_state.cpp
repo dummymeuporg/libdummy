14c86b2f0104fac1da39cc138556138291837d16
Geoffrey ROYER <geoffrey.royer@gmail.com>
2019-06-07 00:56:28 +0200

    Refactoring

--- src/server/game_session_state/loading_state.cpp
+++ src/server/game_session_state/loading_state.cpp
@@ -1,9 +1,12 @@
 #include "server/abstract_game_server.hpp"
 #include "server/game_session.hpp"
+#include "server/command/command.hpp"
+#include "server/command/connect_command.hpp"
 #include "server/game_session_state/loading_state.hpp"
 #include "server/game_session_state/playing_state.hpp"
 #include "server/player.hpp"
 #include "server/map.hpp"
+#include "server/response/connect_response.hpp"
 
 
 namespace Dummy {
@@ -20,6 +23,12 @@ LoadingState::LoadingState(GameSession& gameSession,
 
 void LoadingState::resume() {
     std::cerr << "[LoadingState]" << std::endl;
+}
+
+
+std::unique_ptr<const ::Dummy::Server::Response::Response>
+LoadingState::onCommand(const ::Dummy::Server::Command::Command& command) {
+    return command.accept(*this);
 }
 
 
