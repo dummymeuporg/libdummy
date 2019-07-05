8184c677345b3b600ebf08d7de9640a83cebc560
Geoffrey ROYER <geoffrey.royer@gmail.com>
2019-07-05 15:03:16 +0200

    Update

--- include/core/blocking_layer.hpp
+++ include/core/blocking_layer.hpp
@@ -5,9 +5,20 @@
 
 namespace Dummy {
 namespace Core {
-class BlockingLayer : public std::vector<std::uint8_t>{
+class BlockingLayer : public std::vector<std::uint8_t> {
 public:
     BlockingLayer(std::uint16_t = 1, std::uint16_t = 1);
+
+    std::uint16_t width() const {
+        return m_width;
+    }
+
+    std::uint16_t height() const {
+        return m_height;
+    }
+private:
+    std::uint16_t m_width;
+    std::uint16_t m_height;
 };
 
 } // namespace Core
--- src/core/blocking_layer.cpp
+++ src/core/blocking_layer.cpp
@@ -4,7 +4,10 @@ namespace Dummy {
 namespace Core {
 
 BlockingLayer::BlockingLayer(std::uint16_t width, std::uint16_t height)
-    : std::vector<std::uint8_t>(width * height * 4) {}
+    : std::vector<std::uint8_t>(width * height * 4),
+      m_width(width*2),
+      m_height(height*2)
+{}
 
 } // namespace Core
 } // namespace Dummy
