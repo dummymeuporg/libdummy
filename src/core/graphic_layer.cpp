22af455cc740a08c289204c9fba31f666233ad80
Geoffrey ROYER <geoffrey.royer@gmail.com>
2019-07-05 22:27:50 +0200

    Bugfix

--- src/core/graphic_layer.cpp
+++ src/core/graphic_layer.cpp
@@ -4,7 +4,8 @@ namespace Dummy {
 namespace Core {
 
 GraphicLayer::GraphicLayer(std::uint16_t width, std::uint16_t height)
-    : std::vector<std::pair<std::int8_t, std::int8_t>>(width * height)
+    : std::vector<std::pair<std::int8_t, std::int8_t>>(width * height),
+      m_width(width), m_height(height)
 {}
 
 
