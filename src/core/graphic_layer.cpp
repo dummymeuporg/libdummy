86996fb86f8c513e5751e03dea53bd4b909ce148
Geoffrey ROYER <geoffrey.royer@gmail.com>
2019-09-04 13:20:31 +0200

    Set negative coordinates as default for layers

--- src/core/graphic_layer.cpp
+++ src/core/graphic_layer.cpp
@@ -6,7 +6,10 @@ namespace Core {
 GraphicLayer::GraphicLayer(std::uint16_t width, std::uint16_t height)
     : std::vector<std::pair<std::int8_t, std::int8_t>>(width * height),
       m_width(width), m_height(height)
-{}
+{
+    std::pair<std::int8_t, std::int8_t> reset {-1, -1};
+    std::fill(begin(), end(), reset);
+}
 
 
 } // namespace Core
