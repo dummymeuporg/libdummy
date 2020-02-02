174a36b31f270c3cde0856eb8563b292571c718e
PtiLuky <luky.herbin@free.fr>
2020-02-02 12:13:57 +0100

    pragma once

--- include/dummy/server/server_response_handler.hpp
+++ include/dummy/server/server_response_handler.hpp
@@ -1,4 +1,4 @@
-#pragma
+#pragma once
 
 namespace Dummy {
 namespace Server {
--- include/utils.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <array>
-#include <string>
-
-std::string displayHash(const std::array<unsigned int, 5>& hash);
-void hexdump(const uint8_t* data, size_t size);
